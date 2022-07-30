2de9c286483ddd3e8479625aa519c1897e084e16
diego andres vanegas patiño <tomyvanegas93@gmail.com>
2022-07-30 00:14:54 -0500

    feat: solucion total al momento 1

--- src/Controllers/ModulosController.js
+++ src/Controllers/ModulosController.js
@@ -6,45 +6,68 @@ const ModuloControllers = {};
 ModuloControllers.create = (req, res) => {
     //logica 
 
-    let pesoActual = {}
+    let pesoActual = []
     let pierdenPeso=0
     let gananPeso=0
-    let pierdenPesoIeI=0
     let lograronObj=0
-    let compPeso = {}
-    let pesoDef
+    let resp = {}
+   
 
 
     let pacientes = req.body;
+
     for (let i=0; i < pacientes.length; i++ ) {
             
-        let dato = pacientes[i]
+        let dato = pacientes[i];
 
-        let objpac = {}
+        let objEstadoPeso = {}
 
         if(dato.peso_final > dato.peso_inicial){
-            objpac.nombre=dato.nombre
-            objpac
+            objEstadoPeso["nombre"]=dato.nombre
+            objEstadoPeso["peso_q_subio"]= dato.peso_final - dato.peso_inicial
+            objEstadoPeso["estado"]="gano peso"
+            pesoActual.push(objEstadoPeso)
+        }
+        if(dato.peso_final < dato.peso_inicial){
+            objEstadoPeso["nombre"]=dato.nombre
+            objEstadoPeso["peso_q_subio"]= dato.peso_final - dato.peso_inicial
+            objEstadoPeso["estado"]="perdio peso"
+            pesoActual.push(objEstadoPeso)
+
+        }if(dato.peso_inicial > dato.peso_final && dato.objetivo == "bajar"){
+
+            lograronObj ++
+
+        }
+        if(dato.peso_inicial < dato.peso_final && dato.objetivo == "subir"){
+
+            lograronObj ++
+
+        }
+        if(dato.peso_inicial < dato.peso_intermedio){
+            
             gananPeso ++
+
         }
-        else{
+        if(dato.peso_inicial > dato.peso_intermedio){
+            
             pierdenPeso ++
+
         }
+       
             
             
     }
     
-    res.json({pierdenPeso,gananPeso,pesoDef})   
-   
-    
-}
- 
-  let calculaDEF = (peso_inicial , peso_intermedio , peso_final) =>{
-       let pesoFinal = peso_inicial + peso_intermedio  + peso_final ;
-    
-    return pesoFinal
+    resp["pesoActual"]=pesoActual
+    resp["Subiero_peso"]= gananPeso
+    resp["bajaron_peso"]= pierdenPeso
+    resp["cumpieron_objetivo"]=lograronObj
+    res.json(resp);   
 
 }
+
+
 
 
 module.exports = ModuloControllers;
